c63f3551d046c8eb3e12cb77c786af48ba86e61f
Angela Chin <achin@pivotal.io>
2019-03-01 15:29:17 -0800

    add prompt for seeing current k8s cluster
    [#164321452]

--- git-prompt-colors.sh
+++ git-prompt-colors.sh
@@ -7,8 +7,12 @@ override_git_prompt_colors() {
   DateTime="\$(date +'%Y-%m-%d %H:%M')"
   CfTarget="\$(cf-target)"
   GoBoshTarget="\$(env | grep BOSH_DIR | cut -d '/' -f 7)"
+    KubeCluster="\$(
+  if [ -f ~/.kube/config ]; then
+    kubectl config current-context
+  fi)"
   Ochre="\033[38;5;95m"
-  GIT_PROMPT_START_USER="\n${Ochre}bosh: ${GoBoshTarget} | cf: ${CfTarget} (\h) ${ResetColor}\n${Yellow}${PathShort}${ResetColor}"
+  GIT_PROMPT_START_USER="\n${Ochre}bosh: ${GoBoshTarget} | kube-cluster: ${KubeCluster} | cf: ${CfTarget} (\h) ${ResetColor}\n${Yellow}${PathShort}${ResetColor}"
   GIT_PROMPT_END_USER=" ${Cyan}${GIT_PAIR}${ResetColor}\n$ "
   GIT_PROMPT_END_ROOT="\n# "
 
