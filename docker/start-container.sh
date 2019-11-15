1c893ccbfa717ff8a56e25b11aa700831980f5e8
Edwin Xie <exie@pivotal.io>
2019-11-15 18:46:21 +0000

    Add kind and docker in docker to docker workspace

--- docker/start-container.sh
+++ docker/start-container.sh
@@ -4,7 +4,8 @@ docker run --rm -d \
   -v $SSH_AUTH_SOCK:/ssh-agent \
   --env SSH_AUTH_SOCK=/ssh-agent \
   -p 4567:4567 \
-  -v ~/workspace:/home/pivotal/workspace $@ \
+  -v ~/workspace:/home/pivotal/workspace \
+  -v /var/run/docker.sock:/var/run/docker.sock $@ \
   --cap-add NET_ADMIN --device /dev/net/tun \
   --name docker-workspace \
   --hostname work-machine \
